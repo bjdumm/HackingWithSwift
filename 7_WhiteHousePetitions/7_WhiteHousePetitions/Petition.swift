d9b23b64940862e6d547431e12722fa6e75da4e5
Brennan <bjdumm23@gmail.com>
2023-02-01 09:24:33 -0500

    Finished Project 7

---
+++ 7_WhiteHousePetitions/7_WhiteHousePetitions/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  7_WhiteHousePetitions
+//
+//  Created by Brennan Dumm on 2/1/23.
+//
+
+import Foundation
+
+struct Petition : Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+    
+}
---
+++ 7_WhiteHousePetitions/7_WhiteHousePetitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  7_WhiteHousePetitions
+//
+//  Created by Brennan Dumm on 2/1/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
