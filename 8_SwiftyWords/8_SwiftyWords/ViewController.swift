0fbd28b1a3264838189433e6366f8aade4e09631
Brennan Dumm <bjdumm23@gmail.com>
2023-02-01 11:26:55 -0500

    Added Project 8

---
+++ 8_SwiftyWords/8_SwiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8_SwiftyWords
+//
+//  Created by Brennan Dumm on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
