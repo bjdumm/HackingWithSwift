7c2c173751f1b220ce4627d7069d4dd92ffa3e18
Brennan Dumm <bjdumm23@gmail.com>
2023-02-02 14:29:03 -0500

    Started Project 10

---
+++ 10_NameToFaces/10_NameToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  10_NameToFaces
+//
+//  Created by Brennan Dumm on 2/2/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+}
---
+++ 10_NameToFaces/10_NameToFaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10_NameToFaces
+//
+//  Created by Brennan Dumm on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
