2412d77cd6c752a40f4c4bca70ddffc525bb0221
Brennan Dumm <bjdumm23@gmail.com>
2023-02-03 12:54:15 -0500

    Update Project 12

---
+++ 12a_UserDefaults/10_NameToFaces/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  10_NameToFaces
+//
+//  Created by Brennan Dumm on 2/2/23.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        //Read from disk
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    //Write to disk
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+}
---
+++ 12a_UserDefaults/10_NameToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  10_NameToFaces
+//
+//  Created by Brennan Dumm on 2/2/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+   
+}
